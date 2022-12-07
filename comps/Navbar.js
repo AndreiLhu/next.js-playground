9304f0557ea273b8a065b3be207c9b1583abb287
AndreiLhu <leahu.andreilucian@gmail.com>
2022-12-07 15:56:27 +0100

    adding title to home and about page

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -1,10 +1,11 @@
 import Link from "next/link";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
     <nav>
       <div className="logo">
-        <h1>Ninja List</h1>
+        <Image src="/blacklogo.png" width={120} height={100} />
       </div>
       {/* adding {" "} after link tag because link do not accept the <a></a> at the same time */}
       <Link href="/">
